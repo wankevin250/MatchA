67dcc3d841e3682cc99dbca497a8f8b68c5b44d2
Se-Bin Bin Lee <seblee@seas.upenn.edu>
2022-12-06 20:09:02 +0000

    adsorption backbone2 + tokenize backbone

--- src/main/java/rank/rankJob.java
+++ src/main/java/rank/rankJob.java
@@ -12,12 +12,14 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.lang.Long;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -128,29 +130,40 @@ public class rankJob {
 		return sinks;
 	}
 
-	JavaPairRDD<String,String> getCategoryArticleEdge(String tablename) {
+	JavaPairRDD<String,String> getCategoryArticleEdge() {
+		String tablename = "news";
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-		ScanRequest scanRequest = new ScanRequest()
-    								.withTableName(tablename);
-		ScanResult scanResult = client.scan(scanRequest);
-		List<String[]> rowOfNews = scanResult.getItems().parallelStream()
-						.map(line -> {
-							String[] news = new String[2];
-							news[0] = line.get("category").getS();
-							news[1] = line.get("headline").getS();
-							System.out.println(news[1]);
-							return news;// Make Row with Schema
-						})
-						.collect(Collectors.toList());
-		JavaRDD<String[]> inArr = context.parallelize(rowOfNews);
+		DynamoDB dynamoDB = new DynamoDB(client);
+		Table table = dynamoDB.getTable(tablename);
+		LocalDate date = LocalDate.now().minusYears(5);
+		ZoneId zoneId = ZoneId.systemDefault();
+		long epochToday = date.atStartOfDay(zoneId).toEpochSecond();
+
+		QuerySpec spec = new QuerySpec()
+			.withKeyConditionExpression("date > :v_today")
+			.withValueMap(new ValueMap()
+				.withInt(":v_today", 0)); //change it back to string format data and use the sent link
+
+		ItemCollection<QueryOutcome> items = table.query(spec);
+		List<String[]> friends = new ArrayList<>();
+		Iterator<Item> iter = items.iterator();
+		while (iter.hasNext()) {
+			Item f = iter.next();
+			String[] row = new String[2];
+			row[0] = f.get("acceptor").toString();
+			row[1] = f.get("asker").toString();
+			friends.add(row);
+		}
+		JavaRDD<String[]> inArr = context.parallelize(friends);
 		JavaPairRDD<String, String> result = inArr
 											.mapToPair(x -> new Tuple2<String, String>(x[0], x[1]));
 		
 		return result;
+		
 	}
 
-	JavaPairRDD<String, String> getInteretsEdge(String tablename) {
-		//String tablename = "interest";
+	JavaPairRDD<String, String> getInteretsEdge() {
+		String tablename = "interest";
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
 		ScanRequest scanRequest = new ScanRequest()
     								.withTableName(tablename);
@@ -158,7 +171,7 @@ public class rankJob {
 		List<String[]> rowOfInterest = scanResult.getItems().parallelStream()
 						.map(line -> {
 							String[] news = new String[2];
-							news[0] = line.get("user").getS();
+							news[0] = line.get("username").getS();
 							news[1] = line.get("interest").getS();
 							System.out.println(news[1]);
 							return news;// Make Row with Schema
@@ -171,8 +184,30 @@ public class rankJob {
 		return result;
 	}
 
-	JavaPairRDD<String, String> getFriendsEdge(String tablename) {
-		//String tablename = "friends";
+	JavaPairRDD<String, String> getNewsLikeEdge() {
+		String tablename = "likeNews";
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+		ScanRequest scanRequest = new ScanRequest()
+    								.withTableName(tablename);
+		ScanResult scanResult = client.scan(scanRequest);
+		List<String[]> rowOfInterest = scanResult.getItems().parallelStream()
+						.map(line -> {
+							String[] news = new String[2];
+							news[0] = line.get("username").getS();
+							news[1] = line.get("headline").getS();
+							System.out.println(news[1]);
+							return news;// Make Row with Schema
+						})
+						.collect(Collectors.toList());
+		JavaRDD<String[]> inArr = context.parallelize(rowOfInterest);
+		JavaPairRDD<String, String> result = inArr
+											.mapToPair(x -> new Tuple2<String, String>(x[0], x[1]));
+		
+		return result;
+	}
+
+	JavaPairRDD<String, String> getFriendsEdge() {
+		String tablename = "friends";
 		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
 		DynamoDB dynamoDB = new DynamoDB(client);
 
@@ -203,18 +238,73 @@ public class rankJob {
 
 
 	public void run() {
-		String name = "news";
-		JavaPairRDD<String, String> CAEdge = getCategoryArticleEdge(name);
+		JavaPairRDD<String, String> CategoryNewsEdge = getCategoryArticleEdge();
+		JavaPairRDD<String, String> UserEdge = getFriendsEdge();
+		JavaPairRDD<String, String> InterestEdge = getInteretsEdge();
+		JavaPairRDD<String, String> NewsLikeEdge = getNewsLikeEdge();
+		JavaPairRDD<String, Double> categoryNodeWeight = getCategoryWeight(categoryEdgeURL);
+
+		JavaPairRDD<String, String> allUserEdge = UserEdge
+				.union(UserEdge.mapToPair(x-> new Tuple2<String, String>(x._2, x._1)))
+				.distinct();
+		
+		JavaPairRDD<String, String> allNewsEdge = NewsLikeEdge
+				.union(CategoryNewsEdge.mapToPair(x -> new Tuple2<String, String>(x._2, x._1)))
+				.distinct();
+
+		JavaPairRDD <String, String> edgePairs = CategoryNewsEdge
+				.union(CategoryNewsEdge.mapToPair(x -> new Tuple2<String, String>(x._2, x._1)))
+				.union(UserEdge)
+				.union(UserEdge.mapToPair(x-> new Tuple2<String, String>(x._2, x._1)))
+				.union(InterestEdge)
+				.union(InterestEdge.mapToPair(x-> new Tuple2<String, String>(x._2, x._1)))
+				.distinct();
+
+		JavaPairRDD<String, Tuple2<String, Double>> catEdgeTransfer = CategoryNewsEdge
+				.join(categoryNodeWeight);
+
+		JavaPairRDD<String, Tuple2<String, Double>> newsEdgeTransfer =  allNewsEdge
+				.join(allNewsEdge
+					.mapToPair(x -> new Tuple2<String, Integer>(x._1, 1)) 
+					.reduceByKey((x, y) -> x + y)
+					.mapToPair(x -> new Tuple2<String, Double>(x._1, (10.0 * x._2 / 3.0))));
+
+		JavaPairRDD<String, Tuple2<String, Double>> userFriendEdgeTransfer = allUserEdge
+				.join(allUserEdge
+					.mapToPair(x -> new Tuple2<String, Integer>(x._1, 1)) 
+					.reduceByKey((x, y) -> x + y)
+					.mapToPair(x -> new Tuple2<String, Double>(x._1, (10.0 * x._2 / 4.0))));
+
+		JavaPairRDD<String, Tuple2<String, Double>> userInterestEdgeTransfer = InterestEdge
+				.join(InterestEdge
+					.mapToPair(x -> new Tuple2<String, Integer>(x._1, 1)) 
+					.reduceByKey((x, y) -> x + y)
+					.mapToPair(x -> new Tuple2<String, Double>(x._1, (10.0 * x._2 / 3.0))));
+		
+		JavaPairRDD<String, Tuple2<String, Double>> userNewsEdgeTransfer =  NewsLikeEdge
+				.join(NewsLikeEdge
+					.mapToPair(x -> new Tuple2<String, Integer>(x._1, 1)) 
+					.reduceByKey((x, y) -> x + y)
+					.mapToPair(x -> new Tuple2<String, Double>(x._1, (10.0 * x._2 / 3.0))));
+					
+
+		int iMax = 15;
+		int count = 0;
+		double dMax = 30;		
+		double delta = Integer.MAX_VALUE; 
+
+		while (delta > dMax && count < iMax) {
+			
+		}
+		
 		
 	}
 
 	public void shutdown() {
 		DynamoConnector.shutdown();
-		
 		if (spark != null)
 			spark.close();
 	}
-
 
 	public static void main(String[] args) {
 		final rankJob ln = new rankJob();
